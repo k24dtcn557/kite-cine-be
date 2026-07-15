1d45e0589aede6f9118b2f1d4f4e56142ec36e34
Hung Huynh <hung.hp666@gmail.com>
2026-07-15 07:12:23 +0700

    update cors

--- src/main/java/vn/id/hph/kitecine/service/AuthenticationService.java
+++ src/main/java/vn/id/hph/kitecine/service/AuthenticationService.java
@@ -74,7 +74,7 @@ public class AuthenticationService {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         var user = userRepository
                 .findByUsername(request.getUsername())
-                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+                .orElseThrow(() -> new AppException(ErrorCode.UNAUTHENTICATED));
 
         boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
 
