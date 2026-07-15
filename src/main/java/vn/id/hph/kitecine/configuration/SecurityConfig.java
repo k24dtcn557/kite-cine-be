bb8a3f183ac8923452f5ce0531b1ecbd196e7a79
Hung Huynh <hung.hp666@gmail.com>
2026-07-15 07:04:10 +0700

    update cors

--- src/main/java/vn/id/hph/kitecine/configuration/SecurityConfig.java
+++ src/main/java/vn/id/hph/kitecine/configuration/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
                 .permitAll()
+                .requestMatchers(HttpMethod.OPTIONS)
+                .permitAll()
                 .anyRequest()
                 .authenticated());
 
