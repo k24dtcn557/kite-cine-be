3d8c48a10f1165ef9b74db32b931f1554a6d52e4
Hung Huynh <hung.hp666@gmail.com>
2026-07-23 09:07:06 +0700

    add search user

---
+++ src/main/java/vn/id/hph/kitecine/controller/param/UserSearchParam.java
@@ -0,0 +1,17 @@
+package vn.id.hph.kitecine.controller.param;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserSearchParam extends PageSizeSearchParam {
+    String keyword;
+}
